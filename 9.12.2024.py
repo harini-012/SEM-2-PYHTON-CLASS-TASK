68c8fd61237585f584c4ac3b78d62d91f26d2543
harini-012 <harinirajesh0519@gmail.com>
2024-12-10 19:30:54 +0530

    Update 9.12.2024.py

--- 9.12.2024.py
+++ 9.12.2024.py
@@ -1,29 +1,38 @@
 
-#hybrid inheritance
+#HYBRID INHERITANCE
 class Employee:
-    def __init__(self,emp_id):
-        self.emp_id=emp_id
-    def display_emp(self):
-        print("EMPLOYEE ID",self.emp_id)
+    def _init_(self,name,age):
+        self.name=name
+        self.age=age
+    def displayEmployeeInfo(self):
+        print("Name of the employee:",self.name)
+        print("Age of the employee:",self.age)
 class Manager(Employee):
-    def __init__(self,m_id):
-        self.m_id=m_id
-    def display_man(self):
-        print("Manger id:",self.m_id)
-class Owner(Employee):
-    def __init__(self,own_id):
-        self.own_id=own_id
-    def display_own(self):
-        print("Owner ID",self.own_id)
-class Company(Employee,Manager,Owner):
-    def __init__(self,emp_id,m_id,own_id):
-        Employee.__init__(self,emp_id)
-        Manager.__init__(self,m_id)
-        Owner.__init__(self,own_id)
-    def display_com(self):
-        self.display_emp()
-        self.display_man()
-        self.display_own()
-        
-s=Company("e24ai012","m24ai012","o24ai012")
-s.display_com()
+    def _init_(self,name,age,eid):
+        Employee._init_(self,name,age)
+        self.eid=eid
+    def displayManagerInfo(self):
+        self.displayEmployeeInfo()
+        print("ID:",self.eid)
+class Developer(Employee):
+    def _init_(Self,name,age,dept):
+        Employee._init_(self,name,age)
+        self.dept=dept
+    def displayDeveloperInfo(self):
+        self.displayEmployeeInfo()
+        print("Department:",self.dept)
+class TeamLeader(Manager,Developer):
+    def _init_(self,name,age,eid,dept,teamsize):
+         Employee._init_(self,name,age)
+         self.eid=eid
+         self.dept=dept
+         self.teamsize=teamsize
+    def displayTeamInfo(self):
+        print("Team size={self.Teamsize}")
+Name=input()#we can use different cases here
+Age=int(input())
+Eid=input()
+Dept=input()
+ts=input()
+tl=TeamLeader(Name,Age,Eid,Dept,ts)
+tl.displayEmployeeInfo()
