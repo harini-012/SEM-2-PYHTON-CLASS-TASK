df43c4c3d2852c1d5d53bcae24f837bf5162a850
harini-012 <harinirajesh0519@gmail.com>
2025-01-02 12:19:15 +0530

    Add files via upload

---
+++ 02.1.2025.py
@@ -0,0 +1,24 @@
+from abc import ABC,abstractmethod
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
+class Rectangle:
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+    def area(self):
+        return self.width*self.height
+    def perimeter(self):
+        return 2*(self.width+self.height)
+w=int(input())
+h=int(input())
+r=Rectangle(w,h)
+print("Area=",r.area())
+print("Permieter=",r.perimeter())
+s=Shape()
+
+
