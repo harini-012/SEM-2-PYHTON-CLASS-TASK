dc2fac9065863419ee15078157daddafccc309fd
harini-012 <harinirajesh0519@gmail.com>
2025-02-10 21:56:34 +0530

    Add files via upload

---
+++ 10.02.2025.py
@@ -0,0 +1,58 @@
+
+#print the pattern
+'''+ + + + + 
+    + 
+    + 
++ + + + + 
+    + 
+    + 
++ + + + + 
+'''
+
+rows=int(input())
+cols=int(input())
+
+for i in range(1,rows+1):
+    for j in range(1,cols+1):
+        if i==1 or i==4 or i==7 or j==cols :
+            print("+",end=" ")
+        else:
+            print(" ",end="")
+    print()
+
+#print the pattern
+'''
++ + + + + 
+        + 
+        + 
++ + + + + 
+        + 
+        + 
++ + + + + '''
+
+rows=int(input())
+cols=int(input())
+for i in range(1,rows+1):
+    for j in range(1,cols+1):
+        if i==1 or i==4 or i==7 or j==cols :
+            print("+",end=" ")
+        else:
+            print("  ",end="")
+    print()
+
+
+#print the pattern
+
+rows=int(input())
+cols=int(input())
+for i in range(1,rows+1):
+    for j in range(1,cols+1):
+        if i==1:
+            print):
+
+
+
+
+
+
+        
